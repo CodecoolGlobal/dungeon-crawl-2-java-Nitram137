590c13322861805768213880f9b6518d60f83521
Nitram137 <nemethmartin99@gmail.com>
2022-02-22 15:05:29 +0100

    Implement combat basics

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -3,9 +3,14 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Skeleton extends Actor {
+    private int damage;
+
     public Skeleton(Cell cell) {
         super(cell);
+        damage = -2;
     }
+
+    public int getDamage() { return damage; }
 
     @Override
     public String getTileName() {
