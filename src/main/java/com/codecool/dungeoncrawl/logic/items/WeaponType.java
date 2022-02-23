158e54f9e379badbddee3e73b81f7ad6cfcc75f9
Cseplonorbert <cseplo.norbert.95@gmail.com>
2022-02-23 16:40:21 +0100

    Using Weapon class instead of sword. Implement WeaponType enum.

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/WeaponType.java
@@ -0,0 +1,28 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+public enum WeaponType {
+    THUNDERFURY("Thunderfury, Blessed Blade of the Windseeker", 60),
+    FROSTMOURNE("Frostmourne", 500),
+    SCYTHE("The Grim Reaper", 30),
+    MJOLNIR("Mjolnir", 60),
+    STORMBREAKER("Stormbreaker", 100),
+    STICK("Stick of Truth", 7);
+
+
+    private final String name;
+    private final int damage;
+
+    WeaponType(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+}
