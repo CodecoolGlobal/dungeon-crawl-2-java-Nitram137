727c5cfb270139582f520c98eb209484af01678e
Cseplonorbert <cseplo.norbert.95@gmail.com>
2022-02-23 07:54:43 +0100

    Implement pickUpItem method.

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import java.util.HashMap;
 
 public class Player extends Actor {
     public Player(Cell cell) {
@@ -10,4 +11,16 @@ public class Player extends Actor {
     public String getTileName() {
         return "player";
     }
+
+    public void pickUpItem() {
+        HashMap<String, Integer> inventory = this.getInventory();
+        String itemName = this.getCell().getItem().getClass().getSimpleName();
+        if (inventory.containsKey(itemName)) {
+            Integer counter = inventory.get(itemName);
+            inventory.put(itemName, counter + 1);
+        } else {
+            inventory.put(itemName, 1);
+        }
+    }
+
 }
