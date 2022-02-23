ab622417cc56e9e8d672e3187a161f2edb720d90
Cseplonorbert <cseplo.norbert.95@gmail.com>
2022-02-23 16:41:49 +0100

    Implement KeyType enum.

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Key.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Key.java
@@ -1,15 +1,24 @@
 package com.codecool.dungeoncrawl.logic.items;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.actors.Player;
 
 public class Key extends Item{
 
-    public Key(Cell cell) {
+    KeyType type;
+
+    public Key(Cell cell, KeyType type) {
         super(cell);
+        this.type = type;
+    }
+
+    @Override
+    public void useItem(Player player) {
+
     }
 
     @Override
     public String getTileName() {
-        return "key";
+        return type.getName();
     }
 }
