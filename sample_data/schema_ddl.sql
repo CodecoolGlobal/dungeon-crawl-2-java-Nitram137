599bb4f7ddfbdc0c03bb02e0b1346cd95b568818
Nitram137 <nemethmartin99@gmail.com>
2022-03-08 10:56:28 +0100

    Invert connection between game state and inventory

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -3,7 +3,8 @@ CREATE TABLE public.game_state (
     id serial NOT NULL PRIMARY KEY,
     current_map text NOT NULL,
     saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    player_id integer NOT NULL
+    player_id integer NOT NULL,
+    inventory_id integer NOT NULL
 );
 
 DROP TABLE IF EXISTS public.player;
@@ -18,7 +19,6 @@ CREATE TABLE public.player (
 DROP TABLE IF EXISTS public.inventory;
 CREATE TABLE public.inventory (
     id serial NOT NULL PRIMARY KEY,
-    game_state_id integer NOT NULL,
     iron_key integer,
     potion integer,
     big_potion integer,
@@ -32,5 +32,5 @@ CREATE TABLE public.inventory (
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
-ALTER TABLE ONLY public.inventory
-    ADD CONSTRAINT fk_game_state_id FOREIGN KEY (game_state_id) REFERENCES public.game_state(id);
+ALTER TABLE ONLY public.game_state
+    ADD CONSTRAINT fk_inventory_id FOREIGN KEY (inventory_id) REFERENCES public.inventory(id);
