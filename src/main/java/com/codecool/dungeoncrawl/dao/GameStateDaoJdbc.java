5171da9bf1d70e442a576053771e72fc8db86e0a
Nitram137 <nemethmartin99@gmail.com>
2022-03-08 14:14:26 +0100

    Implement player database queries

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -9,6 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
+
+    private DataSource dataSource;
+
+    public GameStateDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void add(GameState state) {
 
