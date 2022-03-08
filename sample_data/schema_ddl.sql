f92d295f3c7b2a01ad3d08dce7fd1be255d5e1ab
Nitram137 <nemethmartin99@gmail.com>
2022-03-08 10:41:25 +0100

    Customize database schema

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -15,5 +15,22 @@ CREATE TABLE public.player (
     y integer NOT NULL
 );
 
+DROP TABLE IF EXISTS public.inventory;
+CREATE TABLE public.inventory (
+    id serial NOT NULL PRIMARY KEY,
+    game_state_id integer NOT NULL,
+    iron_key integer,
+    potion integer,
+    big_potion integer,
+    Thunderfury boolean,
+    Mjolnir boolean,
+    The_Grim_Reaper boolean,
+    Stormbreaker boolean,
+    Frostmourne boolean,
+    Stick_of_Truth boolean
+);
+
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+ALTER TABLE ONLY public.inventory
+    ADD CONSTRAINT fk_game_state_id FOREIGN KEY (game_state_id) REFERENCES public.game_state(id);
