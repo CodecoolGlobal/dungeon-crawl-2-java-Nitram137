3d7186778cf9c0a3a7db09cace41590459171dc9
Nitram137 <nemethmartin99@gmail.com>
2022-03-09 10:19:54 +0100

    Implement game map to string method

--- src/main/java/com/codecool/dungeoncrawl/logic/GameMap.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/GameMap.java
@@ -4,6 +4,7 @@ import com.codecool.dungeoncrawl.logic.actors.Actor;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameMap {
@@ -51,5 +52,65 @@ public class GameMap {
 
     public int getHeight() {
         return height;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder mapAsString = new StringBuilder();
+        for(Cell[] cellRow : cells) {
+            for(Cell cell : cellRow) {
+                if(cell.getActor() != null)
+                switch (cell.getActor().getTileName()) {
+                    case "player": mapAsString.append("@");
+                        break;
+                    case "skeleton": mapAsString.append("s");
+                        break;
+                    case "shadow": mapAsString.append("S");
+                        break;
+                    case "nuclear cloud": mapAsString.append("N");
+                        break;
+                }
+                else if(cell.getItem() != null)
+                switch (cell.getItem().getTileName()) {
+                    case "Thunderfury": mapAsString.append("t");
+                        break;
+                    case "Mjolnir": mapAsString.append("m");
+                        break;
+                    case "The Grim Reaper": mapAsString.append("c");
+                        break;
+                    case "Stormbreaker": mapAsString.append("T");
+                        break;
+                    case "Frostmourne": mapAsString.append("F");
+                        break;
+                    case "Stick of Truth": mapAsString.append("Q");
+                        break;
+                    case "Iron Key": mapAsString.append("k");
+                        break;
+                    case "Potion": mapAsString.append("p");
+                        break;
+                    case "Big Potion": mapAsString.append("P");
+                        break;
+                }
+                else
+                switch (cell.getTileName()) {
+                    case "empty": mapAsString.append(" ");
+                        break;
+                    case "wall": mapAsString.append("#");
+                        break;
+                    case "forest": mapAsString.append("%");
+                        break;
+                    case "floor": mapAsString.append(".");
+                        break;
+                    case "closed_door": mapAsString.append("D");
+                        break;
+                    case "opened_door": mapAsString.append("d");
+                        break;
+                    case "stairs": mapAsString.append("L");
+                        break;
+                    default: throw new RuntimeException("Missing tile type");
+                }
+            }
+        }
+        return mapAsString.toString();
     }
 }
