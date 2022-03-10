4317696138f8bbe01f4d0a747f5ff2acf7005568
Cseplonorbert <cseplo.norbert.95@gmail.com>
2022-03-10 18:03:35 +0100

    Implement playerAttackEnemyThanPlayerAndEnemyLoseHealth test method.

---
+++ src/test/java/com/codecool/dungeoncrawl/logic/PlayerTest.java
@@ -0,0 +1,32 @@
+package com.codecool.dungeoncrawl.logic;
+
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    GameMap gameMap;
+    Player player;
+
+    @BeforeEach
+    void setUp() {
+        gameMap = new GameMap(3, 3, CellType.FLOOR);
+        player = new Player(gameMap.getCell(1,1));
+    }
+
+    @Test
+    void playerAttackEnemyThanPlayerAndEnemyLoseHealth() {
+        Skeleton skeleton = new Skeleton(gameMap.getCell(1,2));
+        player.move(0,1);
+        int exceptedPlayerHealth = 45;
+        int exceptedSkeletonHealth = 10;
+
+        assertEquals(exceptedPlayerHealth, player.getHealth());
+
+        assertEquals(exceptedSkeletonHealth, skeleton.getHealth());
+    }
+}
